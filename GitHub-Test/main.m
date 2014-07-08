e357700b4d9935db8b11866a1bb3811590bc51f8
RonnieTsang <ronnieos@vip.qq.com>
2014-07-08 23:58:20 +0800

    Initial Commit

---
+++ GitHub-Test/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GitHub-Test
+//
+//  Created by RonnieTsang on 7/8/14.
+//  Copyright (c) 2014 ___RONNIETSANG___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
